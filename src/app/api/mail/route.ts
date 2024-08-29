db8f93586fe6e060ec4880a7798243ce2649e62a
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-08-29 08:09:17 +0700

    chore: Update email template for better styling and responsiveness

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -54,69 +54,80 @@ export async function POST(request: NextRequest) {
       html: `
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Email Template</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            width: 100%;
-            max-width: 600px;
-            margin: 20px auto;
-            background-color: #ffffff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        .header {
-            background-color: #0073e6;
-            padding: 10px;
-            color: #ffffff;
-            text-align: center;
-            border-radius: 8px 8px 0 0;
-        }
-        .content {
-            margin: 20px 0;
-        }
-        .content h2 {
-            color: #333333;
-            font-size: 24px;
-            margin-bottom: 10px;
-        }
-        .content p {
-            color: #555555;
-            font-size: 16px;
-            line-height: 1.5;
-        }
-        .footer {
-            margin-top: 20px;
-            text-align: center;
-            color: #999999;
-            font-size: 12px;
-        }
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+
+      .body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+        padding: 32px 16px;
+      }
+
+      .container {
+        width: 100%;
+        max-width: 600px;
+        margin: 20px auto;
+        background-color: #ffffff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      }
+      .header {
+        background-color: #0073e6;
+        padding: 10px;
+        color: #ffffff;
+        text-align: center;
+        border-radius: 8px 8px 0 0;
+      }
+      .content {
+        margin: 20px 0;
+      }
+      .content h2 {
+        color: #333333;
+        font-size: 24px;
+        margin-bottom: 10px;
+      }
+      .content p {
+        color: #555555;
+        font-size: 16px;
+        line-height: 1.5;
+      }
+      .footer {
+        margin-top: 20px;
+        text-align: center;
+        color: #999999;
+        font-size: 12px;
+      }
     </style>
-</head>
-<body>
-    <div class="container">
+  </head>
+  <body>
+    <div class="body">
+      <div class="container">
         <div class="header">
-            <h1>${title}</h1>
+          <h1>${title}</h1>
         </div>
         <div class="content">
-            <p><strong>From:</strong> ${from}</p>
-            <h2>Message</h2>
-            <p>${message}</p>
+          <p><strong>From:</strong> ${from}</p>
+          <h2>Message</h2>
+          <p>${message}</p>
         </div>
         <div class="footer">
-            <p>&copy; 2024 Sueksit Vachirakumthorn. All rights reserved.</p>
+          <p>&copy; 2024 Sueksit Vachirakumthorn. All rights reserved.</p>
         </div>
+      </div>
     </div>
-</body>
+  </body>
 </html>
       `,
       sender: mailUser,
