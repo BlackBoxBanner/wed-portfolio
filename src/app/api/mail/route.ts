731d3f10986a87ed335c85651ff84b562d1f71ba
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-08-29 08:32:53 +0700

    chore: Update email template to include message body from separate lines

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -15,6 +15,14 @@ export async function POST(request: NextRequest) {
 
     const {from, title, message} = validationResult.data;
 
+    const messages = message.split("\n").filter((sentence) => sentence != "");
+
+    const messageBody = messages
+      .map((text) => {
+        return `<p>${text}</p>`;
+      })
+      .join("");
+
     // Check environment variables and handle errors if not provided
     const mailHost = process.env.MAIL_HOST;
     const mailPort = process.env.MAIL_PORT
@@ -52,85 +60,84 @@ export async function POST(request: NextRequest) {
       to: mailTo,
       subject: title,
       html: `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Email Template</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-      }
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Email Template</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+          }
 
-      .body {
-        font-family: Arial, sans-serif;
-        background-color: #f4f4f4;
-        margin: 0;
-        padding: 0;
-        padding: 32px 16px;
-      }
+          .body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            padding: 32px 16px;
+          }
 
-      .container {
-        width: 100%;
-        max-width: 600px;
-        margin: 20px auto;
-        background-color: #ffffff;
-        padding: 20px;
-        border-radius: 8px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-      .header {
-        background-color: #0073e6;
-        padding: 10px;
-        color: #ffffff;
-        text-align: center;
-        border-radius: 8px 8px 0 0;
-      }
-      .content {
-        margin: 20px 0;
-      }
-      .content h2 {
-        color: #333333;
-        font-size: 24px;
-        margin-bottom: 10px;
-      }
-      .content p {
-        color: #555555;
-        font-size: 16px;
-        line-height: 1.5;
-        padding: 16px;
-      }
-      .footer {
-        margin-top: 20px;
-        text-align: center;
-        color: #999999;
-        font-size: 12px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="body">
-      <div class="container">
-        <div class="header">
-          <h1>${title}</h1>
+          .container {
+            width: 100%;
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+          .header {
+            background-color: #0073e6;
+            padding: 10px;
+            color: #ffffff;
+            text-align: center;
+            border-radius: 8px 8px 0 0;
+          }
+          .content {
+            margin: 20px 0;
+          }
+          .content h2 {
+            color: #333333;
+            font-size: 24px;
+            margin-bottom: 10px;
+          }
+          .content p {
+            color: #555555;
+            font-size: 16px;
+            line-height: 1.5;
+            padding: 16px;
+          }
+          .footer {
+            margin-top: 20px;
+            text-align: center;
+            color: #999999;
+            font-size: 12px;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="body">
+          <div class="container">
+            <div class="header">
+              <h1>${title}</h1>
+            </div>
+            <div class="content">
+              <p><strong>From:</strong> ${from}</p>
+              ${messageBody}
+            </div>
+            <div class="footer">
+              <p>&copy; 2024 Sueksit Vachirakumthorn. All rights reserved.</p>
+            </div>
+          </div>
         </div>
-        <div class="content">
-          <p><strong>From:</strong> ${from}</p>
-          <h2>Message</h2>
-          <p>${message}</p>
-        </div>
-        <div class="footer">
-          <p>&copy; 2024 Sueksit Vachirakumthorn. All rights reserved.</p>
-        </div>
-      </div>
-    </div>
-  </body>
-</html>
-      `,
+      </body>
+    </html>
+          `,
       sender: mailUser,
     };
 
